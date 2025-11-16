c9ddd66bb04a1b8f31b0258006c21eecf5eb6fd4
kaitong <kaitongg@andrew.cmu.edu>
2025-11-15 22:31:00 -0500

    we have a good interface now

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./frontend/**/*.{html,js}", // 扫描 frontend 目录下的所有 HTML/JS 文件
+    "./src/**/*.{html,js}"       // 扫描 src 目录下的文件
+  ],
+  theme: {},
+  plugins: [],
+}
+
